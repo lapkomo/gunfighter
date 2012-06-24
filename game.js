09b291c1c3ecf5d044ba336b63c1a660605118e4
lapkomo <lapkomo@yahoo.co.jp>
2012-06-24 14:00:28 +0900

    game.jsを更新
    タイムを追加

--- game.js
+++ game.js
@@ -1,28 +1,53 @@
 enchant();
-window.onload = function(){
 
-	//ゲームオブジェクトの生成
-	var game = new Game(320,320);
-	game.fps = 16;
-	
+// リミット
+var LIMIT_TIME = 30;
+
+window.onload = function()
+{
+    // ゲームクラスを生成
+    var game = new Game(320, 320);
+    game.fps = 16;
+   
 	//画像の読み込み
-	game.preload('http://enchantjs.com/assets/images/chara2.gif','http://enchantjs.com/assets/images/map0.gif');
+	game.preload('http://enchantjs.com/assets/images/chara2.gif');
 
 	//ロード開始時に呼ばれる
 	game.onload = function(){
+        // タイム
+        var time_label = new Label();
+        time_label.x = time_label.y = 2;
+        time_label.addEventListener(enchant.Event.ENTER_FRAME, function(){
+            var count = Math.floor(game.frame/game.fps);
+            time = LIMIT_TIME - count;
+            this.text = "タイムリミット : " + time;
+            if (time <= 0) { game.end(time, 'GameOver'); }
+        });
+        game.rootScene.addChild(time_label);
+       
 	
-		//背景の生成
-		var bg = new Sprite(320,320);
-		var maptip = game.assets['http://enchantjs.com/assets/images/map0.gif'];
-		var image = new Surface(320,320);
-		for(var j = 0; j < 320; j++){
-			for(var i = 0; i < 320; i++){
-				image.draw(maptip,0,1,16,16,j,i,16,16);
-			}
-		}
-		bg.image = image;
-		game.rootScene.addChild(bg);
+		//背景色の生成
+		game.rootScene.backgroundColor = '#F4A460';
+
+	
+		//敵の生成
+		for(var i=0;i<8;i++){
+			var enemy = new Sprite(32,32);
+			enemy.image = game.assets['http://enchantjs.com/assets/images/chara2.gif'];
+			enemy.moveTo =(150,150);
+			game.rootScene.addChild(enemy);
+		};
 		
+		//敵をタッチした時の処理
+		enemy.addEventListener("touchstart",function(){
+			game.rootScene.removeChild(enemy);
+		});
+		
+		//敵をタッチし終わった時の処理
+		enemy.addEventListener("touchend",function(){
+			enemy.moveTo = Math.floor(Math.random()*(320-32), Math.random()*(320-32));
+			game.rootScene.addChild(enemy);
+		});
 	};
 	
 	//ゲーム開始
