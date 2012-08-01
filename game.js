bea6affb6e0a4eccad233479139c072cdd823f3a
lapkomo <lapkomo@yahoo.co.jp>
2012-08-01 11:15:02 +0900

    #3 スコアの表示と加算 終了

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ enchant();
 // リミット
 var LIMIT_TIME = 30;
 var game;
-var addsocre = 10;
+var addscore = 10;
 
 		// ここで自作クラスEnemyをつくる
 		var Enemy = Class.create(Sprite, // Spriteクラスを継承
@@ -15,7 +15,7 @@ var addsocre = 10;
                         this.dy = 3;
                         this.frame = 0;
                         this.tick = 0;
-                        this.anim  = [0, 1, 0, 2];
+                        // this.anim  = [0, 1, 0, 2];
                         game.rootScene.addChild(this);
                       },
                       //enterframeイベントのリスナーを定義する
@@ -30,7 +30,7 @@ var addsocre = 10;
                         }
                         
                         //xが320-32以上になったら跳ね返りする
-                        if(this.x >= 320-32){
+                        if(this.x >= 320 - 32){
                         	this.dx *= -1;
                         }
                         //xが0になったら跳ね返りする
@@ -38,17 +38,17 @@ var addsocre = 10;
                         	this.dx *= -1;
                         }
                         
-                        
                         //scaleXが7以上になったらスプライトをremove
-                      	if(this.scaleX >= 7.5){
+                      	if(this.scaleX >= 3.5){
                       		game.rootScene.removeChild(this);
                       	}
                         
-                      },
-                     ontouchend: function(){
-                      	game.rootScene.removeChild(this);
-                        game.score = game.score + addscore;
-                      }
+                      	},
+                      	//タッチが終了した時の処理
+                     	ontouchend: function(){
+                        	game.score = game.score + addscore;
+                      		game.rootScene.removeChild(this);
+                      	}
                     });
                     
 
@@ -58,12 +58,11 @@ window.onload = function(){
     game.fps = 16;
     game.score = 0;
 	//画像の読み込み
-	game.preload('gunfighter.png','wilderness.gif','crag.gif','cactus.gif','fcactus.gif','rock.gif','bullet.gif');
+	game.preload('gunfighter.png','wilderness.gif','crag.gif','cactus.gif','fcactus.gif','rock.gif','bullet.gif','heart.png');
 	//ロード開始時に呼ばれる
 	game.onload = function(){
 	
-	
-			//背景画像の生成
+		//背景画像の生成
 		var bg = new Sprite(320,320);
 		bg.image = game.assets['wilderness.gif'];
 		game.rootScene.addChild(bg);
@@ -71,10 +70,10 @@ window.onload = function(){
 	
 		//enemyを複数表示させる
 		for (var i = 0; i < 5; i++) {
-            enemyi = new Enemy(Math.random() * 320 - 32, Math.random() * 320 - 32); 
+            enemyi = new Enemy(Math.floor(Math.random() * 280 - 32) + 32, Math.floor(Math.random() * 280 - 32) + 32); 
         }
 	
-      // タイム
+      	// タイム
         var time_label = new Label();
         time_label.x = 2;
         time_label.y = 2;
@@ -82,47 +81,50 @@ window.onload = function(){
             var count = Math.floor(game.frame/game.fps);
             time = LIMIT_TIME - count;
             time_label.text = "タイムリミット : " + time;
-            if (time <= 0) { game.end(time, 'GameOver'); }
+            if (time <= 0) { game.end(game.score, 'GameOver'); }
         });
         game.rootScene.addChild(time_label);
    
-      //スコア
+      	//スコア
         var score_label = new Label();
         score_label.x = 2;
         score_label.y = 12;
-        score_label.text = "スコア：" + game.score;
+        score_label.addEventListener(enchant.Event.ENTER_FRAME,function(){
+        	score_label.text = "スコア：" + game.score;
+        });
         game.rootScene.addChild(score_label);
    
-   
-
+		//ライフを表示
+		var life = new Sprite(16,16);
+		life.image = game.assets['heart.png'];
+		life.x = 300; life.y = 2;
+		game.rootScene.addChild(life);	
+		
 		
 		//障害物を表示
-		var barrier1 = new Sprite(64,64);
-		barrier1.image = game.assets['crag.gif'];
-		barrier1.x = 200;
-		barrier1.y = 100;
-		game.rootScene.addChild(barrier1);
+		var obstacle1 = new Sprite(64,64);
+		obstacle1.image = game.assets['crag.gif'];
+		obstacle1.x = 200;
+		obstacle1.y = 100;
+		game.rootScene.addChild(obstacle1);
 		
-		var barrier2 = new Sprite(64,64);
-		barrier2.image = game.assets['rock.gif'];
-		barrier2.x = 100;
-		barrier2.y = 150;
-		game.rootScene.addChild(barrier2);
+		var obstacle2 = new Sprite(64,64);
+		obstacle2.image = game.assets['rock.gif'];
+		obstacle2.x = 100;
+		obstacle2.y = 150;
+		game.rootScene.addChild(obstacle2);
 		
-		var barrier3 = new Sprite(64,64);
-		barrier3.image = game.assets['cactus.gif'];
-		barrier3.x = 100;
-		barrier3.y = 200;
-		game.rootScene.addChild(barrier3);
+		var obstacle3 = new Sprite(64,64);
+		obstacle3.image = game.assets['cactus.gif'];
+		obstacle3.x = 200;
+		obstacle3.y = 200;
+		game.rootScene.addChild(obstacle3);
 		
-		var barrier4 = new Sprite(64,64);
-		barrier4.image = game.assets['fcactus.gif'];
-		barrier4.x = 80;
-		barrier4.y = 80;
-		game.rootScene.addChild(barrier4);
-	
-
-
+		var obstacle4 = new Sprite(64,64);
+		obstacle4.image = game.assets['fcactus.gif'];
+		obstacle4.x = 80;
+		obstacle4.y = 80;
+		game.rootScene.addChild(obstacle4);
 		
 	};
 	//ゲーム開始
